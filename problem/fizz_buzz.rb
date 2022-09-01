d3b7c6c5a2ea7304f9733f449fc8991f34d0b341
sota-fujimoto <112251582+itsamojotofu@users.noreply.github.com>
2022-09-01 21:06:09 +0900

    add cases up to 15

--- problem/fizz_buzz.rb
+++ problem/fizz_buzz.rb
@@ -17,5 +17,9 @@ class FizzBuzzTest < Minitest::Test
     assert_equal 1, fizz_buzz(1)
     assert_equal 2, fizz_buzz(2)
     assert_equal 'Fizz', fizz_buzz(3)
+    assert_equal 4, fizz_buzz(4)
+    assert_equal 'Buzz', fizz_buzz(5)
+    assert_equal 'Fizz', fizz_buzz(6)
+    assert_equal 'Fizz Buzz', fizz_buzz(15)
   end
 end
