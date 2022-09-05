4c081920bfaa45fc6a08eaaac56ae73cf920dcb2
sota-fujimoto <112251582+itsamojotofu@users.noreply.github.com>
2022-09-05 16:07:42 +0900

    try senior block technique

--- demo/ease_with_symbols.rb
+++ demo/ease_with_symbols.rb
@@ -1,3 +1,8 @@
 # an exmple block
 program_languages = ['ruby', 'java', 'python']
 puts program_languages.map {|n| n.upcase}
+
+puts '------'
+
+# senior technique
+puts program_languages.map(&:upcase)
