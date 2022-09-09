408da40eb40aea5e6e9ae9fcffdfc8e5c42a5f6f
sota-fujimoto <112251582+itsamojotofu@users.noreply.github.com>
2022-09-09 11:07:43 +0900

    easier than inequality

--- demo/range.rb
+++ demo/range.rb
@@ -8,3 +8,11 @@ puts (1...5).include?(5)
 num = [1,2,3,4,5]
 
 puts num[1..3]
+
+def water_liquid?(temp)
+  (0...100).include?(temp)
+end
+
+puts water_liquid?(-1)
+puts water_liquid?(50)
+puts water_liquid?(100)
