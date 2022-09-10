fb7f1140e562b9777353da15101c2661c7123699
sota-fujimoto <112251582+itsamojotofu@users.noreply.github.com>
2022-09-10 15:52:27 +0900

    check if test code works

--- lib/rgb_conversion.rb
+++ lib/rgb_conversion.rb
@@ -1,6 +1,9 @@
 # Spec requirements
 # define two methods: one is to convert Decimal to Hexadecimal, the other is to convert Hexadecimal to Decimal
 
+def to_hex(r,g,b)
+  '#0000000'
+end
 
 to_hex(0,0,0)
 
--- test/rgb_test.rb
+++ test/rgb_test.rb
@@ -1,8 +1,9 @@
 # try TDD test driven development
 
 require 'minitest/autorun'
+require_relative '../lib/rgb_conversion'
 
-class RgbTest < Minitest::test
+class RgbTest < Minitest::Test
   def test_to_hex
     assert_equal '#000000', to_hex(0,0,0)
   end
