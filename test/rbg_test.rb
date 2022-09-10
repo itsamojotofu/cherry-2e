39691a4def48a10137ef2247f2901f9f647a8ecc
sota-fujimoto <112251582+itsamojotofu@users.noreply.github.com>
2022-09-10 15:42:56 +0900

    create rgbtest class

--- test/rbg_test.rb
+++ test/rbg_test.rb
@@ -1,1 +1,9 @@
-# try TDD test driven developmetn
+# try TDD test driven development
+
+require 'minitest/autorun'
+
+class RgbTest < Minitest::test
+  def test_to_hex
+    assert_equal '#000000', to_hex(0,0,0)
+  end
+end
