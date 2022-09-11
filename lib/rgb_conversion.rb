f6afcb8362ca5a48c200bc846aae9a9795496b8e
sota-fujimoto <112251582+itsamojotofu@users.noreply.github.com>
2022-09-11 10:58:00 +0900

    refactoring: replace each with sum

--- lib/rgb_conversion.rb
+++ lib/rgb_conversion.rb
@@ -2,9 +2,7 @@
 # define two methods: one is to convert Decimal to Hexadecimal, the other is to convert Hexadecimal to Decimal
 
 def to_hex(r,g,b)
-  hex = '#'
-  [r,g,b].each do |n|
-    hex += n.to_s(16).rjust(2,'0')
+  [r,g,b].sum('#') do |n|
+    n.to_s(16).rjust(2,'0')
   end
-  hex
 end
