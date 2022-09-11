65adda3ab12d65db66ed7f0b5398d49c5ca9df57
sota-fujimoto <112251582+itsamojotofu@users.noreply.github.com>
2022-09-12 00:52:32 +0900

    remove error case

--- test/rgb_test.rb
+++ test/rgb_test.rb
@@ -7,8 +7,6 @@ class RgbTest < Minitest::Test
   def test_to_hex
     assert_equal '#000000', to_hex(0,0,0)
     assert_equal '#ffffff', to_hex(255,255,255)
-    # error case
-    assert_equal '#110c32', to_hex(100,40,15)
   end
   def test_to_ints
     assert_equal [0,0,0], to_ints('#000000')
