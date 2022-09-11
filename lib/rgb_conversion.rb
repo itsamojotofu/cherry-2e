cf2a6c184b51b276d2449c212df1058c4a11597a
sota-fujimoto <112251582+itsamojotofu@users.noreply.github.com>
2022-09-11 10:57:02 +0900

    refactoring along DRY

--- lib/rgb_conversion.rb
+++ lib/rgb_conversion.rb
@@ -2,8 +2,9 @@
 # define two methods: one is to convert Decimal to Hexadecimal, the other is to convert Hexadecimal to Decimal
 
 def to_hex(r,g,b)
-  '#' +
-  r.to_s(16).rjust(2,'0') +
-  g.to_s(16).rjust(2,'0') +
-  b.to_s(16).rjust(2,'0')
+  hex = '#'
+  [r,g,b].each do |n|
+    hex += n.to_s(16).rjust(2,'0')
+  end
+  hex
 end
--- test/rgb_test.rb
+++ test/rgb_test.rb
@@ -7,5 +7,7 @@ class RgbTest < Minitest::Test
   def test_to_hex
     assert_equal '#000000', to_hex(0,0,0)
     assert_equal '#ffffff', to_hex(255,255,255)
+    # error case
+    assert_equal '#110c32', to_hex(100,40,15)
   end
 end
