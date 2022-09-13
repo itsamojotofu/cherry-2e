28d80b45e61e9d56e50b9c0177de069dabe79b9b
sota-fujimoto <112251582+itsamojotofu@users.noreply.github.com>
2022-09-14 03:26:20 +0900

    try Set class

--- demo/set_with_array.rb
+++ demo/set_with_array.rb
@@ -6,3 +6,15 @@ puts a|b # sum
 puts a-b # diffrence
 puts "----"
 puts a&b # common
+
+# Set is more effective than Array
+
+require 'set'
+
+a = Set[1,2,3]
+b = Set[3,4,5]
+
+puts a|b # sum
+puts a-b # diffrence
+puts "----"
+puts a&b # common
