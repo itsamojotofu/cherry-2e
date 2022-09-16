ca1490862ceb02bcb3002a9a2e58c761d0a2e56c
sota-fujimoto <112251582+itsamojotofu@users.noreply.github.com>
2022-09-17 00:17:13 +0900

    try variadic method

--- demo/array_in_depth.rb
+++ demo/array_in_depth.rb
@@ -55,3 +55,13 @@ case food
 when *fruits # = when 'grape', 'orange' 
    puts 'tasty!'
 end
+
+# variadic arguments
+def greet(*names)
+   "#{names.join('と')}、こんにちは！"
+end
+
+greet('田中さん')
+greet('田中さん','鈴木さん')
+greet('田中さん','鈴木さん','佐藤さん')
+
