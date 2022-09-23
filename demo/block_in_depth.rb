158ccdbdfca98bfca4eefa294bdb245191214756
sota-fujimoto <112251582+itsamojotofu@users.noreply.github.com>
2022-09-24 02:29:10 +0900

    enumerator object is returned

--- demo/block_in_depth.rb
+++ demo/block_in_depth.rb
@@ -9,4 +9,7 @@ fruits.map.with_index {|f,i| puts "#{i}:#{f}"}
 # with_index can be combined with others
 fruits.delete_if.with_index {|f,i| f.include?('a')} # => lemon
 
-
+# loop method return enumerator object
+fruits = ['apple','lemon','grape']
+p fruits.each
+p fruits.delte_if
