a223dd14e9649ed2770e2f6d741d18bbc964bf6c
itsamojotofu <112251582+itsamojotofu@users.noreply.github.com>
2022-11-04 08:11:09 +0900

    use symbol instead of str

--- lib/convert_length.rb
+++ lib/convert_length.rb
@@ -1,4 +1,4 @@
 def convert_length(length, unit_from, unit_to)
-  units = {'m' => 1.0, 'ft' => 3.28, 'in' => 39.37}
+  units = { m: 1.0, ft: 3.28, in: 39.37}
   (length/ units[unit_from] * units[unit_to]).round(2)
 end
