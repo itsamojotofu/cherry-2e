0314e49cad064757f618015062f5221c2f940e95
itsamojotofu <112251582+itsamojotofu@users.noreply.github.com>
2022-11-04 08:06:32 +0900

    add third test

--- test/convert_length_test.rb
+++ test/convert_length_test.rb
@@ -3,9 +3,9 @@ require_relative '../lib/convert_length'
 
 class ConvertLengthTest < Minitest::Test
   def test_convert_length
-    assert_equal 39.37, convert_length(1,'m','n')
-    assert_equal 0.38, convert_length(15, 'm', 'n')
-    
+    assert_equal 39.37, convert_length(1,'m','in')
+    assert_equal 0.38, convert_length(15, 'in', 'm')
+    assert_equal 10670.73, convert_length(35000, 'ft', 'm')
   end
 end
 
