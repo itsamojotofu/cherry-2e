8626fe715644ef75eac411a2939f879c760d87ba
itsamojotofu <112251582+itsamojotofu@users.noreply.github.com>
2022-11-04 08:00:23 +0900

    Update convert_length_test.rb

--- test/convert_length_test.rb
+++ test/convert_length_test.rb
@@ -4,6 +4,8 @@ require_relative '../lib/convert_length'
 class ConvertLengthTest < Minitest::Test
   def test_convert_length
     assert_equal 39.37, convert_length(1,'m','n')
+    assert_equal 0.38, convert_length(15, 'm', 'n')
+    
   end
 end
 
