c3ea62118f6ee22c77d98768cccb211bd505dfd5
itsamojotofu <112251582+itsamojotofu@users.noreply.github.com>
2022-11-04 08:15:30 +0900

    keyword argument

--- lib/convert_length.rb
+++ lib/convert_length.rb
@@ -1,4 +1,4 @@
-def convert_length(length, unit_from, unit_to)
-  units = { m: 1.0, ft: 3.28, in: 39.37}
-  (length/ units[unit_from] * units[unit_to]).round(2)
+UNITS = { m: 1.0, ft: 3.28, in: 39.37}
+def convert_length(length, from: :m, to: :m)
+  (length/ UNITS[from] * UNITS[to]).round(2)
 end
