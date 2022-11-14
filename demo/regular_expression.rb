b5966620d4e97ee519a7685101ab2ba3abb3d945
itsamojotofu <112251582+itsamojotofu@users.noreply.github.com>
2022-11-15 07:34:41 +0900

    # 6.1.1

--- demo/regular_expression.rb
+++ demo/regular_expression.rb
@@ -1,2 +1,17 @@
 # 6.1
 
+# 6.1.1 - convert hash format from one to the other
+
+# format 1
+{
+  name: "Alice",
+  age: 18,
+  gender: :male
+}
+
+# format 2
+{
+  :name => "Alice",
+  :age => 18,
+  :gender => :male
+}
