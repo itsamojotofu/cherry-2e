8c498238b3b27fadc2aea226596dccd7820f58a9
itsamojotofu <112251582+itsamojotofu@users.noreply.github.com>
2022-11-17 00:47:57 +0900

    6.2.1

--- demo/regular_expression.rb
+++ demo/regular_expression.rb
@@ -32,3 +32,15 @@ TEXT
 #   age: 18,
 #   gender: :male
 # }
+
+# 6.2
+
+# 6.2.1 - know the goodness of regex
+
+text = <<TEXT
+I love Ruby.
+Python is a great language.
+Java and JavaScript are different.
+TEXT
+
+text.scan(/[A-Z][A-Za-z]+/)
