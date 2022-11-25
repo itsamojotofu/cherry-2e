0e75ad4826c01aa3ca3c000f3df7aae29b1b0af9
itsamojotofu <112251582+itsamojotofu@users.noreply.github.com>
2022-11-26 03:05:27 +0900

    add test code

---
+++ test/convert_hash_syntax_test.rb
@@ -0,0 +1,8 @@
+require 'minitest/autorun'
+require_relative '../lib/convert_hash_syntax'
+
+class ConvertHashSyntaxTest < Minitest::Test
+  def test_convert_hash_syntax
+    assert_equal '{}', convert_hash_syntax('{}')
+  end
+end
