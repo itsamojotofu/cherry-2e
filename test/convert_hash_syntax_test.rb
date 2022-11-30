cc17e1c1dd854729bea12179c4b2c0547ced1007
itsamojotofu <112251582+itsamojotofu@users.noreply.github.com>
2022-12-01 07:19:57 +0900

    add old_syntax

--- test/convert_hash_syntax_test.rb
+++ test/convert_hash_syntax_test.rb
@@ -3,7 +3,17 @@ require_relative '../lib/convert_hash_syntax'
 
 class ConvertHashSyntaxTest < Minitest::Test
   def test_convert_hash_syntax
+    old_syntax = <<~TEXT
+    {
+      :name => 'Alice',
+      :age => 20,
+      :gender => :female
+    }
+    TEXT
     
+
+
+
     # assert_equal '{}', convert_hash_syntax('{}')
   end
 end
